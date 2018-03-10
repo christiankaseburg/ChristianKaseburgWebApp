6e4b13041cecd96d1e421afad065ccbd3f5fd11b
Christian Kaseburg <Christiankaseburg@gmail.com>
2018-03-09 17:44:04 -0800

    Experiment mockup done
    Experiment mockup is done, just need to responsive, and add eventlisteners to helper library.

--- ClientApp/app/components/experiments/experiments.component.ts
+++ ClientApp/app/components/experiments/experiments.component.ts
@@ -1,6 +1,6 @@
-﻿import { Component, ElementRef } from '@angular/core';
+﻿import { Component, ElementRef, HostListener } from '@angular/core';
 
-const godraysVideo= require('../../assets/videos/godrays.mp4');
+const godraysVideo = require('../../assets/videos/godrays.mp4');
 
 @Component({
     selector: 'experiments',
@@ -9,48 +9,162 @@ const godraysVideo= require('../../assets/videos/godrays.mp4');
 })
 
 export class ExperimentsComponent {
-    private godrays = godraysVideo;
 
-    public videoWidth: number;
-    public videoHeight: number;
-    public videoContainerWidth: number;
-    public videoContainerHeight: number;
+    private videoRef = { godrays: godraysVideo };
+    private video;
 
     constructor(private element: ElementRef) {
     }
 
-    onResize(event: Event) {
+    onResize() {
+        setTimeout(this.resizeVideo(), 500);
+    }
+
+    onScroll() {
+        setTimeout(this.isVideoVisible(), 200);
+    }
+
+    /*
+    * Fix for loading mp4's on iOS, and android devices.
+    */
+    loadVideo() {
+
+        for (let i = 0; i < 0; i++) {
+            let playPromise = this.video[i].play();
+
+            if (playPromise !== undefined) {
+                playPromise.then(_ => {
+                    // Automatic playback started!
+                    // Show playing UI.
+                    // We can now safely pause video...
+                    this.video[i].pause();
+                })
+                    .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+                    });
+            }
+        }
+    }
+
+    /*
+    * Resize videos to keep a consistent scale across all screen sizes.
+    */
+    resizeVideo() {
         let windowWidth = window.innerWidth,
             windowHeight = window.innerHeight;
         let windowAspectRatio = windowHeight / windowWidth;
 
-        let video = document.getElementById('godrays');
-        let videoWidth = video!.clientWidth;
-        let videoHeight = video!.clientHeight;
-        let videoAspectRatio = videoHeight / videoWidth;
+        let videos = document.getElementsByTagName('video');
+        let carouselSlider = document.getElementById('carousel-slider');
+        let carouselSliderContainer = document.getElementById('carousel-Slider-Container');
+        let seperator = document.getElementById('seperator');
+        let carouselSliderHeight = Math.round(window.innerHeight * .138);
+        let seperatorBottom = Math.round(window.innerHeight * .03);
 
-        //compare window ratio to image ratio so you know which way the image should fill
-        if (windowAspectRatio < videoAspectRatio) {
-            // we are fill width
-            video!.style.width = windowWidth + "px";
-            // and applying the correct aspect to the height now
-            video!.style.height = (windowWidth * videoAspectRatio) + "px";
-            // this can be margin if your element is not positioned relatively, absolutely or fixed
-            // make sure image is always centered
-            video!.style.left = "0px";
-            video!.style.top = (windowHeight - (windowWidth * videoAspectRatio)) / 2 + "px";
-        } else { // same thing as above but filling height instead
-            video!.style.height = windowHeight + "px";
-            video!.style.width = (windowHeight / videoAspectRatio) + "px";
-            video!.style.left = (windowWidth - (windowHeight / videoAspectRatio)) / 2 + "px";
-            video!.style.top = "0px";
+        for (let i = 0; i < videos.length; i++) {
+            let video = videos[i];
+            let videoWidth = video!.clientWidth + Math.random();
+            let videoHeight = video!.clientHeight;
+            let videoAspectRatio = videoHeight / videoWidth;
+
+            //compare window ratio to image ratio so you know which way the image should fill
+            if (windowAspectRatio < videoAspectRatio) {
+                // we are fill width
+                video!.style.width = windowWidth + "px";
+                // and applying the correct aspect to the height now
+                video!.style.height = (windowWidth * videoAspectRatio) + "px";
+            } else { // same thing as above but filling height instead
+                carouselSlider!.style.height = carouselSliderHeight + "px";
+                carouselSliderContainer!.style.height = carouselSliderHeight + "px";
+                seperator!.style.bottom = seperatorBottom + "px";
+                video!.style.height = windowHeight + "px";
+                video!.style.width = (windowHeight / videoAspectRatio) + "px";
+            }
         }
     }
 
+    /*
+    * Play the video that is in the current viewport.
+    * getBoundingClientRect is supported across all browsers, and intersectobserver is not.
+    */
+    isVideoVisible() {
+
+        for (let i = 0; i < this.video.length; i++) {
+
+            let videoRect = this.video[i].getBoundingClientRect();
+            let offset = Math.ceil(videoRect.height / 2);
+            let videoTop = videoRect.top + offset, videoBottom = videoRect.bottom - offset;
+
+            let isVisible = (videoTop >= 0) && (videoBottom <= window.innerHeight);
+
+            if (isVisible) {
+                this.video[i].play();
+            } else {
+                this.video[i].pause();
+            }
+        }
+    }
+
+    /*
+    * Pause videos when viewport is not in focus.
+    */
+    pauseVideos() {
+        Object.keys(this.video).forEach(i => {
+            this.video[i].pause();
+        });
+    }
+
+    horizontalScroll(event: Event) {
+
+        let delta = (<any>event).detail ? (<any>event).detail * (-60) : (<any>event).wheelDelta * (.5); //+60 scroll up, -60 scroll down (Wheel delta returns 120)
+        (<any>event).currentTarget.scrollLeft -= delta;
+
+        event.preventDefault();
+    }
+
+    horizontalDragScroll(event: Event) {
+
+        console.log('mousedown');
+        event.preventDefault();
+    }
+
+    expandCarouselSlider() {
+        let carouselSlider = document.getElementById('carousel-slider');
+        if (carouselSlider!.style.display == "block") {
+            carouselSlider!.style.display = "none";
+        } else {
+            carouselSlider!.style.display = "block";
+        }
+
+    }
+
+    // Server side pre rendering work around
     ngOnInit() {
-        //this.videoWidth = window.innerWidth * 1;
-        //this.videoHeight = window.innerHeight * .7;
-        //this.videoContainerWidth = window.innerWidth - (window.innerWidth * .03);
-        //this.videoContainerHeight = window.innerHeight - (window.innerHeight * .03);
+        let slider = document.getElementById('carousel-slider');
+
+        //mouse scroll
+        if ((slider as any)!.attachEvent) (slider as any)!.attachEvent("on" + (/Gecko\//i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel", this.horizontalScroll);
+        else if (slider!.addEventListener) slider!.addEventListener((/Gecko\//i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel", this.horizontalScroll, false);
+
+        // Drag scroll horizontal. (CONVERT ALL HORIZONTAL SCROLLING INTO A MODULE IN HELPER LIBRARY)
+        let curXPos: number = 0;
+        let prevChangeInX: number = 0;
+        let curDown: boolean = false;
+
+        slider!.addEventListener('mousemove', function(event){ 
+            if (curDown === true) {
+                // not really needed
+                //let delta = prevChangeInX > (curXPos - event.pageX) ? -3 : 3;
+                slider!.scrollLeft -= prevChangeInX - (curXPos - event.pageX);
+                prevChangeInX = (curXPos - event.pageX);
+            }
+        });
+        slider!.addEventListener('mousedown', function (event) { curDown = true; curXPos = event.pageX; prevChangeInX = (curXPos - event.pageX)});
+        document.addEventListener('mouseup', function (event){ curDown = false; }); 
+
+        this.video = document.getElementsByTagName('video');
+        this.resizeVideo();
+        this.loadVideo();
     }
 }
